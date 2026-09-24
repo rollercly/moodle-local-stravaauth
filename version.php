9b307ae442ce387accbeea2b2feb795e7ed251c3
rollercly <rollercly@gmail.com>
2026-09-24 11:15:35 +0200

    Update en version

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_stravaauth';
-$plugin->version   = 2026091000;      // YYYYMMDDXX
+$plugin->version   = 2026092400;      // YYYYMMDDXX
 $plugin->requires  = 2022112800;      // Moodle 4.1+
 $plugin->maturity  = MATURITY_ALPHA;
 $plugin->release   = '0.1.0';
